c795f2ab9d132f1b0c8652b165eda12759e75fb4
Felicia Kenney <fkenney@bravocg.com>
2022-07-27 16:15:50 -0400

    added assert throws for exception

--- src/main/java/com/tlglearning/fizzbuzz/model/Analysis.java
+++ src/main/java/com/tlglearning/fizzbuzz/model/Analysis.java
@@ -5,17 +5,32 @@ import java.util.Set;
 
 public class Analysis {
 
-   public Set<State> analyze(int value){
-     Set<State> result = EnumSet.noneOf(State.class);
+  private static final String ILLEGAL_VALUE_FORMAT = "Value was %,d; must be non-negative.";
 
-     if(value % 3 == 0){
-       result.add(State.FIZZ);
-     }
+  /**
+   *
+   * @param value
+   * @return
+   * @throws IllegalArgumentException if {@code value < 0}.
+   */
+  public Set<State> analyze(int value) throws IllegalArgumentException {
 
-     if(value % 5 == 0){
-       result.add(State.BUZZ);
-     }
-     return result;
-   }
+    if (value < 0) {
+      throw new IllegalArgumentException(String.format(ILLEGAL_VALUE_FORMAT, value));
+    }
+
+    Set<State> result = EnumSet.noneOf(State.class);
+
+    if (value % 3 == 0) {
+      result.add(State.FIZZ);
+    }
+
+    if (value % 5 == 0) {
+      result.add(State.BUZZ);
+    }
+
+    return result;
+
+  }
 
 }
